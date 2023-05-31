005d8bfba774baa18a552267c0ea19b38b08a662
Airu <rizkykirigaya@gmail.com>
2023-05-31 22:22:22 +0800

    283. Move Zeroes

---
+++ 283. Move Zeroes.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = 0; j < nums.length - 1; j++) {
+      if (nums[j] == 0) [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+    }
+  }
+}
